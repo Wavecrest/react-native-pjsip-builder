e1fb2d3835fbb74987cdb55cca0b5c4eb00927cc
sergey-shablenko <37369710+sergey-shablenko@users.noreply.github.com>
2024-08-28 14:42:07 +0300

    Feat update ndk (#1)
    * feat: update ndk
    
    * feat: updating build tool versions
    
    * fix: refactor to support buildx
    
    * feat: update swig, turn off processor optimisations to fix compile errors
    
    * fix: update readme
    
    * fix: attempting to solve compilation errors
    
    * fix: update openssl to 1.1.1, update API to 29
    
    * fix: wrong ndk path, drop x86-64
    
    * fix: build open-ssl as static library
    
    ---------
    
    Co-authored-by: sergey.shablenko <sergey.s@thoughtandfunction.com>

--- README.md
+++ README.md
@@ -2,19 +2,28 @@
 Easily build PJSIP with: OpenSSL, OpenH264, Opus and G.729 for Android and iOS, by using Docker and xCode.
 
 ## Versions
-| Library              | Version |
-|----------------------|---------|
-| Android API          | 23      |
-| Android NDK          | r12b    |
-| PJSIP                | 2.7.1   |
-| OPENSSL              | 1.0.2g  |
-| OPENH264             | 1.7.0   | 
-| OPUS                 | 1.2.1   |
+| Library      | Version   |
+|--------------|-----------|
+| Android API  | 29        |
+| Android NDK  | r21e      |
+| PJSIP        | 2.9.0     |
+| OPENSSL      | 1.0.2g    |
+| ~~OPENH264~~ | ~~1.7.0~~ | 
+| OPUS         | 1.2.1 (?) |
 
 ## Build for Android
 ```
 git clone https://github.com/datso/react-native-pjsip-builder
 cd react-native-pjsip-builder; ./build_android
+```
+
+```
+docker buildx build \
+--platform linux/arm64,linux/amd64 \
+--tag yourtag \
+--cache-from=type=local,src=./cache \
+--cache-to=type=local,dest=./cache,mode=max \
+--push .
 ```
 
 ## Build for iOS
--- android/build_opus.sh
+++ android/build_opus.sh
@@ -7,11 +7,42 @@ TARGET_PATH=/output/opus/${TARGET_ARCH}
 cp -r /sources/opus /tmp/opus
 
 cd /tmp/opus/jni
-ndk-build APP_ABI="${TARGET_ARCH}" 
+
+# Valid APP_ABI values: armeabi-v7a, arm64-v8a, x86_64
+if [[ "$TARGET_ARCH" == "armeabi-v7a" || "$TARGET_ARCH" == "arm64-v8a" || "$TARGET_ARCH" == "x86_64" ]]; then
+    APP_ABI="${TARGET_ARCH}"
+else
+    echo "Invalid architecture: $TARGET_ARCH"
+    exit 1
+fi
+
+export APP_PLATFORM=android-29
+# Set the NDK paths and compiler based on the target architecture
+export ANDROID_NDK_ROOT=/sources/android_ndk
+export PATH="$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH"
+
+if [ "$TARGET_ARCH" == "armeabi-v7a" ]; then
+    export CC="armv7a-linux-androideabi29-clang"
+    export CXX="armv7a-linux-androideabi29-clang++"
+elif [ "$TARGET_ARCH" == "arm64-v8a" ]; then
+    export CC="aarch64-linux-android29-clang"
+    export CXX="aarch64-linux-android29-clang++"
+elif [ "$TARGET_ARCH" == "x86_64" ]; then
+    export CC="x86_64-linux-android29-clang"
+    export CXX="x86_64-linux-android29-clang++"
+else
+    echo "Unsupported target architecture: $TARGET_ARCH"
+    exit 1
+fi
+
+# Run ndk-build with the appropriate APP_ABI
+ndk-build APP_ABI="${APP_ABI}"
 
 mkdir -p ${TARGET_PATH}/include
 mkdir -p ${TARGET_PATH}/lib
-cp -r ../include ${TARGET_PATH}/include/opus
-cp ../obj/local/${TARGET_ARCH}/libopus.a ${TARGET_PATH}/lib/
 
-rm -rf /tmp/pjsip
+cp -r ../include ${TARGET_PATH}/include/opus
+cp ../obj/local/${APP_ABI}/libopus.a ${TARGET_PATH}/lib/
+
+# Clean up temporary directory
+rm -rf /tmp/opus
